09ef153f38b27518356bb84eba331fee7549abfe
Crend King <975235+CrendKing@users.noreply.github.com>
2025-03-23 15:06:11 -0700

    Migrate to pyproject

--- css_select.py
+++ css_select.py
@@ -2,10 +2,11 @@
 Parse HTML from stdin and output elements filtered by specified CSS selectors and arguments.
 """
 
-from urllib.parse import urljoin
 import argparse
-import bs4
 import sys
+from urllib.parse import urljoin
+
+import bs4
 
 args_parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 args_parser.add_argument('selector')
@@ -17,18 +18,18 @@ args = args_parser.parse_args()
 soup = bs4.BeautifulSoup(sys.stdin, 'html.parser')
 results = soup.select(args.selector)
 
-if args.attribute:
 
-    def transform(tag):
+def transform(tag):
+    if args.attribute:
         attr_value = tag.get(args.attribute) or ''
         if args.base and tag.name == 'a' and args.attribute == 'href':
             attr_value = urljoin(args.base, attr_value)
         return attr_value
+    elif args.text:
+        return tag.get_text()
+    else:
+        return str(tag)
 
-elif args.text:
-    transform = lambda tag: tag.get_text()
-else:
-    transform = lambda tag: str(tag)
 
 final = [transform(tag) for tag in results]
 print(*final, sep='\n')
